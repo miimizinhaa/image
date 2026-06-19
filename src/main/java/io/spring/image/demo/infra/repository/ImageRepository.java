8d37cdea891855bcca54b3df8ec3b4c6397b356e
Iasmim Lino <lino.souza@estudante.ifmt.edu.br>
2026-06-19 12:17:46 -0400

    add files

--- src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
+++ src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
@@ -2,12 +2,17 @@ package io.spring.image.demo.infra.repository;
 
 import io.spring.image.demo.domain.entity.Image;
 import io.spring.image.demo.domain.enums.ImageExtension;
+import io.spring.image.demo.infra.repository.specs.GenericSpecs;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
+
+import static io.spring.image.demo.infra.repository.specs.GenericSpecs.*;
+import static io.spring.image.demo.infra.repository.specs.ImageSpecs.*;
+import static org.springframework.data.jpa.domain.Specification.*;
 
 public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
 
@@ -22,22 +27,12 @@ public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecif
      */
 
     default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
-        Specification <Image> conjunction = (root, q, criteriaBuilder) ->criteriaBuilder.conjunction();
-        Specification<Image> spec = Specification.where(conjunction);
+        Specification<Image> spec = where(conjunction());
         if(extension !=null){
-            //AND EXTENSION = 'PNG'
-            Specification<Image> extensionEqual = (root, q, cb) -> cb.equal(root.get("extension"), extension);
-            spec = spec.and(extensionEqual);
+            spec = spec.and(extensionEqual(extension));
         }
         if(StringUtils.hasText(query)){
-            //AND (NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY')
-            //Specification<Image> nameLike = (root, q, cb)-> {};
-            //Specification<Image> tagsLike = (root, q, cb)-> {};
-            Specification<Image> nameLike = (root, q, cb)-> cb.like(cb.upper(root.get("name")),"%"+query.toUpperCase()+"%");
-            Specification<Image> tagsLike = (root, q, cb)-> cb.like(cb.upper(root.get("tags")),"%"+query.toUpperCase()+"%");
-
-            Specification<Image> nameOrTagsLike = Specification.anyOf(nameLike, tagsLike);
-            spec = spec.and(nameOrTagsLike);
+            spec = spec.and(anyOf(nameLike(query), tagsLike(query)));
         }
         return findAll(spec);
     }
