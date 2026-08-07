6d6c63383f1cb87d2a374446fce851c2a67b91ab
Iasmim Lino <lino.souza@estudante.ifmt.edu.br>
2026-08-07 12:57:28 -0400

    Páginas

---
+++ imagelite/app/galeria/page.tsx
@@ -0,0 +1,7 @@
+export default function Galeria() {
+  return(
+    <main>
+      <h1>Galeria</h1>
+    </main>
+  )
+}
---
+++ imagelite/app/questionario/page.tsx
@@ -0,0 +1,9 @@
+export default function Questionario() {
+  return(
+    <div>
+    <main>
+      <h1>Questionario</h1>
+    </main>
+    </div>
+  )
+}
